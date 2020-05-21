269f98d36d335f8ebff9378b834073b56363135b
Dinho Zanardi <prof.dinho.ti@gmail.com>
2020-05-21 15:20:08 -0300

    Incluindo Swagger no projeto

--- backend/src/main/java/com/profdinho/api/controller/EnderecoController.java
+++ backend/src/main/java/com/profdinho/api/controller/EnderecoController.java
@@ -2,6 +2,7 @@ package com.profdinho.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,14 +17,22 @@ import org.slf4j.LoggerFactory;
 import com.profdinho.api.model.Endereco;
 import com.profdinho.api.service.EnderecoService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
+@Api(value = "Endereco")
 public class EnderecoController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(EnderecoController.class);
 	
 	@Autowired EnderecoService enderecoService;
 	
-	@GetMapping(path = "/endereco/{cep}", produces = { "application/json" })
+	@ApiOperation(
+			value = "Buscar um endereço", 
+			response = Endereco.class, 
+			notes = "Essa operação busca um endereço através de um CEP informado.")
+	@GetMapping(path = "/endereco/{cep}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Endereco> buscarEndereco(@PathVariable("cep") String cep) {
 		Endereco endereco = new Endereco();
 		try {
@@ -37,7 +46,11 @@ public class EnderecoController {
 		return ResponseEntity.ok(endereco);
 	}
 	
-	@GetMapping(path = "/endereco/{uf}/{cidade}/{logradouro}", produces = { "application/json" })
+	@ApiOperation(
+			value = "Buscar uma lista de CEPS",
+			response = List.class,
+			notes = "Essa operação busca uma lista de CEPs através de um endereço informado.")
+	@GetMapping(path = "/endereco/{uf}/{cidade}/{logradouro}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<Endereco>> buscarCEP(@PathVariable("uf") String uf, @PathVariable("cidade") String cidade, @PathVariable("logradouro") String logradouro){
 		List<Endereco> listaEnderecos = new ArrayList<Endereco>();
 		try {
